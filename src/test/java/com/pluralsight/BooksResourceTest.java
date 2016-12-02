ed901dcd85e93666b7c3fbe717b3ebf4d1431716
Ian Wells <ian.wells@dwp.gsi.gov.uk>
2016-12-02 13:59:26 +0000

    introduced use of JerseyTest for much better and cleaner testing

--- src/test/java/com/pluralsight/BooksResourceTest.java
+++ src/test/java/com/pluralsight/BooksResourceTest.java
@@ -2,6 +2,8 @@ package com.pluralsight;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.util.HttpStatus;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,7 +12,11 @@ import org.junit.Test;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
+
+import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -19,42 +25,31 @@ import static org.junit.Assert.*;
 /**
  * Created by ianwells on 30/11/2016.
  */
-public class BooksResourceTest {
+public class BooksResourceTest extends JerseyTest {
 
-    private HttpServer server;
-    private WebTarget target;
+    private static final String BOOKS_ENDPOINT = "books";
 
-    @Before
-    public void setUp() throws Exception {
-        // start the server
-        server = Main.startServer();
-        // create the client
-        Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
-        target = c.target(Main.BASE_URI);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        server.stop();
+    @Override
+    protected Application configure() {
+        return new ResourceConfig().packages("com.pluralsight");
     }
 
     @Test
     public void givenBooksResourceNeededThenShouldBeFound() {
-        Response response = target.path("books").request().get();
+        Response response = target(BOOKS_ENDPOINT).request().get();
         Assert.assertThat(response.getStatus(), is(equalTo(HttpStatus.OK_200.getStatusCode())));
     }
 
     @Test
     public void givenBookByIdNeededThenShouldReturnBookWithThatId() {
-        Book result = target.path("/books/1").request().get(Book.class);
+        Book result = target(BOOKS_ENDPOINT + "/1").request().get(Book.class);
         Assert.assertThat(result.getId(), is(equalTo("1")));
+    }
+
+    @Test
+    public void givenCollectionsOfBooksNeededThenShouldReturnCollection() {
+        Collection<Book> response = target(BOOKS_ENDPOINT).request().get(new GenericType<Collection<Book>>() {});
+        assertEquals(2, response.size());
     }
 
 }
--- src/test/java/com/pluralsight/MyResourceTest.java
+++ src/test/java/com/pluralsight/MyResourceTest.java
@@ -1,46 +1,23 @@
 package com.pluralsight;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.grizzly.http.server.HttpServer;
-
 import org.glassfish.grizzly.http.util.HttpStatus;
-import org.junit.After;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertEquals;
 
-public class MyResourceTest {
+public class MyResourceTest extends JerseyTest {
 
-    private HttpServer server;
-    private WebTarget target;
-
-    @Before
-    public void setUp() throws Exception {
-        // start the server
-        server = Main.startServer();
-        // create the client
-        Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
-        target = c.target(Main.BASE_URI);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        server.stop();
+    @Override
+    protected Application configure() {
+        return new ResourceConfig().packages("com.pluralsight");
     }
 
     /**
@@ -48,13 +25,13 @@ public class MyResourceTest {
      */
     @Test
     public void testGetIt() {
-        String responseMsg = target.path("myresource").request().get(String.class);
+        String responseMsg = target("myresource").request().get(String.class);
         assertEquals("Got it!", responseMsg);
     }
 
     @Test
     public void givenPathToNonExistentReseourceThenShouldReturnNotFoundErrorHTTPError() {
-        Response result = target.path("nonexistentresource").request().get();
+        Response result = target("nonexistentresource").request().get();
         Assert.assertThat(result.getStatus(), is(equalTo(HttpStatus.NOT_FOUND_404.getStatusCode())));
     }
 }
